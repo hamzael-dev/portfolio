7a43dd31dc176ef3ebcad67f6a431dc73cfdc1e6
hamza <hamzaelazrak8@gmail.com>
2026-06-28 18:58:53 +0100

    first commit 09

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -72,20 +72,21 @@ font-semibold
         >
 
           Hi, I'm{" "}
-
-          <span
-            className="
-            bg-gradient-to-r
-            from-purple-400
-            via-pink-400
-            to-cyan-400
-            bg-clip-text
-            [-webkit-background-clip:text]
-            [-webkit-text-fill-color:transparent]
-            "
-          >
-            Hamza El Azrak
-          </span>
+<span
+className="
+inline-block
+bg-gradient-to-r
+from-purple-400
+via-pink-400
+to-cyan-400
+bg-clip-text
+text-transparent
+[-webkit-background-clip:text]
+[-webkit-text-fill-color:white]
+"
+>
+Hamza El Azrak
+</span>
 
         </motion.h1>
 
